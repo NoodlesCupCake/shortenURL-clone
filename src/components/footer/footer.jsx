6c6e6e368fb480181ea764e44982834814c1b0b1
Jirayu Klinudom <jizato.jirayu2001@gmail.com>
2024-03-02 04:27:37 +0700

    Add Repo

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,8 @@ import './footer.css'
 function Footer() {
     return (
       <section id="footer">
-        <div id="footer-text">© 2024 Jirayu Klinudom - Tool to shorten a long link (Cloned for Assignment)</div>
+        <div id="footer-text">© 2024 Jirayu Klinudom - A fake cloned tool to shorten a long link (<a id="hyperlink" href="https://github.com/NoodlesCupCake/shortenURL-clone">Check Repository</a>)</div>
+        {/* <a id="hyperlink" href="https://github.com/NoodlesCupCake/shortenURL-clone">Repository</a> */}
       </section>
     )
   }
